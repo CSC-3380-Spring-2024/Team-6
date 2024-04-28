63f9f5952d0b10de70f5b3ed3cf4af63c5759cf8
htpeyton <157847089+htpeyton@users.noreply.github.com>
2024-04-28 17:43:42 -0400

    script

--- Enemies/Vulture.cs
+++ Enemies/Vulture.cs
@@ -3,24 +3,43 @@ using System;
 
 public class Vulture : CharacterBody2D
 {
-	private int speed = 35;
-	private bool playerChase = false;
-	private Node2D player = null;
+	private const int Speed = 100;
+	private const float DetectionRadius = 200;
 
-	private void _on_detection_area_body_entered(Node2D body)
-{
-	player = (Node2D)body;
-		playerChase = true;
+	private Vector2 _velocity = Vector2.Zero;
+	private Node2D _player = null;
+	private Area2D _detectionArea;
+
+	public override void _Ready()
+	{
+		_detectionArea = GetNode("DetectionArea") as Area2D;
+		_detectionArea.Connect("body_entered", this, "_on_detection_area_body_entered");
+		_detectionArea.Connect("body_exited", this, "_on_detection_area_body_exited");
+	}
+
+	public override void _PhysicsProcess(float delta)
+	{
+		if (_player != null)
+		{
+			Vector2 direction = (_player.GlobalPosition - GlobalPosition).Normalized();
+			_velocity = direction * Speed * delta;
+			MoveAndSlide(_velocity);
+		}
+	}
+
+	private void _on_detection_area_body_entered(Node body)
+	{
+		if (body is Player)
+		{
+			_player = (Node2D)body;
+		}
+	}
+
+	private void _on_detection_area_body_exited(Node body)
+	{
+		if (body == _player)
+		{
+			_player = null;
+		}
+	}
 }
-
-
-	
-private void _on_detection_area_body_exited(Node2D body)
-{
-	 player = null;
-		playerChase = false;
-}
-
-	
-
-
